183f9a3de2cb7dec027d44ad04a1083d2c3ca4b1
Arpita Banik <deyarpitabanik@gmail.com>
2025-01-14 11:15:57 +0600

    added api endpont and fetched data

--- src/component/FetchVideos/FetchVideos.js
+++ src/component/FetchVideos/FetchVideos.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
 const FetchVideos = () => {
+    const YOUTUBE_PLAYLISTS_ITEM_API ='https://www.googleapis.com/youtube/v3/playlistItems';
+        const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
+        console.log('object', apiKey);
+        async function fetchYoutubeData() {
+            try {
+                const res = await fetch(
+                    `${YOUTUBE_PLAYLISTS_ITEM_API}?part=snippet&playlistId=UULF9J48NTCqKe74-OCsWxcsQA&maxResults=50&key=${apiKey}`
+                );
+                const data = await res.json();
+                console.log("my data", data);
+                return { data };
+            } catch (error) {
+                return { data: null, error: 'Error fetching data' };
+            }
+        }
+        fetchYoutubeData();
     return (
         <div>
             <h2>fetch videos</h2>
