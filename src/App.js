c5351b9ca1f1395cc4e1af900a174366f65c7cb8
Arpita Banik <deyarpitabanik@gmail.com>
2025-01-14 10:52:25 +0600

    added structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/Navbar/Navbar';
+import FetchVideos from './component/FetchVideos/FetchVideos';
+import Footer from './component/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <FetchVideos/>
+      <Footer/>
     </div>
   );
 }
