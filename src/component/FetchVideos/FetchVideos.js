7ef86056b3fd520c6b289037cf67bfba5dc40184
Arpita Banik <deyarpitabanik@gmail.com>
2025-01-14 11:33:31 +0600

    added hooks

--- src/component/FetchVideos/FetchVideos.js
+++ src/component/FetchVideos/FetchVideos.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const FetchVideos = () => {
+    const [videos, setVideos] = useState([]);
+    const [error, setError] = useState(null);
+
     const YOUTUBE_PLAYLISTS_ITEM_API ='https://www.googleapis.com/youtube/v3/playlistItems';
         const apiKey = process.env.REACT_APP_YOUTUBE_API_KEY;
         console.log('object', apiKey);
-        async function fetchYoutubeData() {
-            try {
-                const res = await fetch(
-                    `${YOUTUBE_PLAYLISTS_ITEM_API}?part=snippet&playlistId=UULF9J48NTCqKe74-OCsWxcsQA&maxResults=50&key=${apiKey}`
-                );
-                const data = await res.json();
-                console.log("my data", data);
-                return { data };
-            } catch (error) {
-                return { data: null, error: 'Error fetching data' };
-            }
-        }
-        fetchYoutubeData();
+        useEffect(() => {
+            const fetchYoutubeData = async () => {
+                try {
+                    const res = await fetch(
+                        `${YOUTUBE_PLAYLISTS_ITEM_API}?part=snippet&playlistId=UULF9J48NTCqKe74-OCsWxcsQA&maxResults=10&key=${apiKey}`
+                    );
+                    const data = await res.json();
+    console.log('object',data);
+                    if (data.items) {
+                        setVideos(data.items);
+                    } else {
+                        setError('No data found');
+                    }
+                } catch (err) {
+                    setError('Error fetching data');
+                }
+            };
+    
+            fetchYoutubeData();
+        }, [apiKey]);
     return (
         <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:p-8">
             <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
