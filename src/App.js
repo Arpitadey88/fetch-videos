327ce716c0bbf2c7aad452f00fc04688f6b71327
Arpita Banik <deyarpitabanik@gmail.com>
2025-01-15 17:14:00 +0600

    added hero banner section

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Navbar from './component/Navbar/Navbar';
 import FetchVideos from './component/FetchVideos/FetchVideos';
 import Footer from './component/Footer/Footer';
 import PexelsData from './component/PexelsData/PexelsData';
+import HeroBanner from './component/HeroBanner/HeroBanner';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <FetchVideos/>
-      <PexelsData/>
+      <HeroBanner/>
+      {/* <FetchVideos/>
+      <PexelsData/> */}
       <Footer/>
     </div>
   );
